5029d13d90cc0e2e8caffcd67bd01e9de76ff474
Zsolt <zsolt.iachmanovski@yahoo.com>
2023-02-01 22:14:00 +0200

    am invatat despre value type

---
+++ src/test/java/ValueType/Jucarie.java
@@ -0,0 +1,22 @@
+package ValueType;
+
+public class Jucarie {
+    public String Nume;
+
+    public Jucarie(String nume) {
+        Nume = nume;
+    }
+
+    public String getNume() {
+        return Nume;
+    }
+
+    public void setNume(String nume) {
+        Nume = nume;
+    }
+
+    public void Info(){
+        System.out.println(Nume);
+    }
+}
+
